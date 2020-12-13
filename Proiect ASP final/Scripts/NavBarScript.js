5ff311ed3cc7856a96dd2cc213bbc729eb009d78
unknown <acalin2000@gmail.com>
2020-12-13 16:15:43 +0200

    frontend ideas

---
+++ Proiect ASP final/Scripts/NavBarScript.js
@@ -0,0 +1,28 @@
+﻿
+if (document.title == "Produse") {
+
+    var prodLink = document.getElementById("linkProd");
+    prodLink.className = "activeLink";
+}
+else if (document.title == "Categorii") {
+
+    var categLink = document.getElementById("linkCateg");
+    categLink.className = "activeLink";
+}
+else if (document.title == "Adrese") {
+
+    var adrLink = document.getElementById("linkAdrese");
+    adrLink.className = "activeLink";
+}
+else if (document.title == "Register") {
+
+    var regLink = document.getElementById("registerLink");
+    regLink.className = "activeLink";
+}
+else if (document.title == "Log in") {
+
+    var logLink = document.getElementById("loginLink");
+    logLink.className = "nav-item active";
+}
+
+
